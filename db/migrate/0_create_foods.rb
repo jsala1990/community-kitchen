0a9db1983ee799f0cffbee31fcde88035b474a79
Jesus <jsalazar@amplify.com>
2015-12-13 21:41:45 -0500

    adding assertions

--- db/migrate/0_create_foods.rb
+++ db/migrate/0_create_foods.rb
@@ -2,6 +2,8 @@ class CreateFoods < ActiveRecord::Migration
   def self.up
     create_table :foods do |t|
       t.string :name
+      t.integer :quantity
+      t.string :unit
     end
   end
 end
--- test/models/foods_spec.rb
+++ test/models/foods_spec.rb
@@ -4,8 +4,10 @@ require './test/spec_helper'
 describe Food do
   describe '#validations' do
     it 'checks for presence of all required attributes' do
-      orange = Food.new name: 'orange'
+      orange = Food.create name: 'orange', quantity: 3, unit: 'lbs'
       assert_equal 'orange', orange.name
+      assert_equal 3, orange.quantity
+      assert_equal 'lbs', orange.unit
     end
   end
 end
