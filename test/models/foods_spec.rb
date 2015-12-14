64e0f9195aa6fb1039c1d094dad00b252cf2a4fa
Jesus <jsalazar@amplify.com>
2015-12-13 21:46:50 -0500

    adding transactions

--- test/models/foods_spec.rb
+++ test/models/foods_spec.rb
@@ -2,12 +2,17 @@ require './lib/models/foods'
 require './test/spec_helper'
 
 describe Food do
+  include WithRollBack
+
   describe '#validations' do
     it 'checks for presence of all required attributes' do
-      orange = Food.create name: 'orange', quantity: 3, unit: 'lbs'
-      assert_equal 'orange', orange.name
-      assert_equal 3, orange.quantity
-      assert_equal 'lbs', orange.unit
+      temporarily do
+        orange = Food.create name: 'orange', quantity: 3, unit: 'lbs'
+        assert_equal 'orange', orange.name
+        assert_equal 3, orange.quantity
+        assert_equal 'lbs', orange.unit
+      end
+      assert_equal 0, Food.count
     end
   end
 end
--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -6,3 +6,12 @@ require 'minitest/autorun'
 require 'sqlite3'
 require 'active_record'
 require './config/environment'
+
+module WithRollBack
+  def  temporarily(&block)
+    ActiveRecord::Base.connection.transaction do
+      block.call
+      raise ActiveRecord::Rollback
+    end
+  end
+end
